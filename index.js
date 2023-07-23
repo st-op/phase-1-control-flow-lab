5913070ee4d19398843c17e8eef2c0003ca4c395
Stella Johansson <stellajoh9@gmail.com>
2023-07-23 17:23:37 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let city
+let distance
+let message 
+let tip
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    message = "This one is on me!"
+  }
+  else if (400 < distance && distance < 2000) {
+    message = "That will be twenty bucks."
+  }
+  else if (distance > 2000 && distance < 2500) {
+    message = "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500) {
+    message = "No can do."
+  }
+  else {
+    message = "Ok, sounds good."
+  }
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city === 'NYC') {
+  message = 'Ok, sounds good.'
+}
+else if (city !== 'NYC') {
+  message = "No go."
+}
+return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    message = "Thank you so much."
+  }
+  else if (tip === 'not as generous') {
+    message = "Thank you."
+  }
+  else {
+    message = "Bye."
+  }
+  return message
 }
+
+console.log(switchOnCharmFromTip("hi"))
